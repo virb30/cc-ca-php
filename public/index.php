8616deb167150f5bf85102185f1324d8272c458c
Vinicius Boscoa <virb30@gmail.com>
2022-03-22 23:23:05 -0300

    add api test

--- public/index.php
+++ public/index.php
@@ -2,6 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use App\Application\UseCase\GetOrders\GetOrders;
+use App\Infra\Database\PdoMysqlConnectionAdapter;
+use App\Infra\Factory\DatabaseRepositoryFactory;
 use App\Infra\Http\Http;
 use App\Infra\Http\SlimHttp;
 use App\Infra\Http\SymfonyHttp;
@@ -10,13 +13,12 @@ use App\Infra\Http\LumenHttp;
 
 function init(Http $http)
 {
-  $http->route('get', '/books', function($params, $body) {
-    $books = [
-      (object) ['title' => 'Clean Code'],
-      (object) ['title' => 'Refactoring'],
-      (object) ['title' => 'Implementing Domain-Driven Design'],
-    ];
-    return $books;
+  $http->route('get', '/orders', function($params, $body) {
+    $connection = new PdoMysqlConnectionAdapter();
+    $repositoryFactory = new DatabaseRepositoryFactory($connection);
+    $getOrders = new GetOrders($repositoryFactory);
+    $output = $getOrders->execute();
+    return $output;
   });
   
   $http->run();
--- tests/Feature/HttpTest.php
+++ tests/Feature/HttpTest.php
@@ -1,18 +1,45 @@
 <?php declare(strict_types=1);
 
 namespace Tests\Feature;
+
+use App\Application\UseCase\PlaceOrder\PlaceOrder;
+use App\Application\UseCase\PlaceOrder\PlaceOrderInput;
+use App\Domain\Factory\RepositoryFactory;
+use App\Infra\Database\Connection;
+use App\Infra\Database\PdoMysqlConnectionAdapter;
+use App\Infra\Factory\DatabaseRepositoryFactory;
 use Tests\TestCase;
 
 class HttpTest extends TestCase
 {
+  private Connection $connection;
+  private RepositoryFactory $repositoryFactory;
+
+  protected function setUp(): void
+  {
+    parent::setUp();
+    $this->connection = new PdoMysqlConnectionAdapter();
+    $this->repositoryFactory = new DatabaseRepositoryFactory($this->connection);
+    $orderRepository = $this->repositoryFactory->createOrderRepository();
+    $orderRepository->clean();
+  }
   public function testShouldTestApi()
   {
-    $response = $this->get('/books');
-    $books = json_decode((string) $response->getBody());
-    
-    $this->assertCount(3, $books);
-    $this->assertEquals('Clean Code', $books[0]->title);
-    $this->assertEquals('Refactoring', $books[1]->title);
-    $this->assertEquals('Implementing Domain-Driven Design', $books[2]->title);
+    $placeOrder = new PlaceOrder($this->repositoryFactory);
+    $input = new PlaceOrderInput(
+      cpf: "935.411.347-80",
+      orderItems: [
+        (object) ['idItem' => 1, 'quantity' => 1],
+        (object) ['idItem' => 2, 'quantity' => 1],
+        (object) ['idItem' => 3, 'quantity' => 3],
+      ],
+      coupon: "VALE20"
+    );
+    $placeOrder->execute($input);
+    $placeOrder->execute($input);
+    $placeOrder->execute($input);
+    $response = $this->get('/orders');
+    $orders = json_decode((string) $response->getBody());
+    $this->assertCount(3, $orders);
   }
 }
